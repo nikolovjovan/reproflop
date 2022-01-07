361206ec6c7c147dc79eee4388c5ce4c165e13ba
Jovan Nikolov <nikolovjovan@hotmail.com>
2022-01-07 23:16:40 +0100

    Start transitioning to CMake (CPU lib only)

--- src/cpu/LongAccumulator/test_base_fn.cpp
+++ src/cpu/LongAccumulator/test_base_fn.cpp
@@ -8,14 +8,28 @@ using namespace std;
 int main()
 {
     LongAccumulator a(8388607.5);
+    float fa = 8388607.5;
+
+    bool is_pass = true;
+
+    is_pass = fa == a();
 
     cout << fixed << setprecision(10);
 
     cout << "a = " << a() << " (" << a << ")\n";
 
     a += 1.255;
+    fa += 1.255;
 
     cout << "a = " << a() << " (" << a << ")\n";
+
+    is_pass = is_pass && (fa == a());
+
+    if (is_pass) {
+        cout << "TestPassed; ALL OK!\n";
+    } else {
+        cout << "TestFailed!\n";
+    }
 
     return 0;
 }
