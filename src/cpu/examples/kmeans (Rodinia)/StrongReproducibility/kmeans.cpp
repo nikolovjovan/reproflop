348f2d79b91a384b1931ece5d9d3d6eae81c1fd8
Jovan Nikolov <nikolovjovan@hotmail.com>
2023-02-11 23:05:14 +0100

    Add kmeans CPU results

--- src/cpu/examples/kmeans (Rodinia)/StrongReproducibility/kmeans.cpp
+++ src/cpu/examples/kmeans (Rodinia)/StrongReproducibility/kmeans.cpp
@@ -80,47 +80,23 @@
 
 #include "kmeans.h"
 
-void usage(char *argv0)
-{
-    char help[] = "Usage: %s [switches] -i filename\n"
-                  "       -i filename     : file containing data to be clustered\n"
-                  "       -b              : input file is in binary format\n"
-                  "       -k              : number of clusters (default is 8)\n"
-                  "       -r              : number of repeats for reproducibilty study (default is 0)\n"
-                  "       -t threshold    : threshold value\n";
-    fprintf(stderr, help, argv0);
-    exit(-1);
-}
+#include <iomanip>
+#include <iostream>
 
-bool diff(int nclusters, int numAttributes, float **cluster_centres_1, float **cluster_centres_2)
-{
-    for (int i = 0; i < nclusters; i++)
-        for (int j = 0; j < numAttributes; j++)
-            if (cluster_centres_1[i][j] != cluster_centres_2[i][j])
-                return true;
-    return false;
-}
+using namespace std;
 
-void run(int nrepeats, bool parallel, bool reproducible, int numObjects, int numAttributes,
-         float **attributes, int nclusters, float threshold, float** &cluster_centres, double &time)
+constexpr char* input_files[] = { "100", "204800.txt", "819200.txt", "kdd_cup" };
+
+void execute(bool parallel, bool reproducible, int numObjects, int numAttributes,
+         float **attributes, int nclusters, float threshold)
 {
-    printf("Running %s (%sreproducible) implementation...\n", parallel ? "parallel" : "sequential", reproducible ? "" : "non-");
-    float **tmp_cluster_centres = NULL;
-    for (int i = 0; i <= nrepeats; i++) {
-        if (i == 0)
-            time = omp_get_wtime();
-        cluster(parallel, reproducible, numObjects, numAttributes, attributes, /* [numObjects][numAttributes] */
-                nclusters, threshold, &tmp_cluster_centres);
-        if (i == 0) {
-            time = omp_get_wtime() - time;
-            cluster_centres = tmp_cluster_centres;
-            tmp_cluster_centres = NULL;
-        } else if (diff(nclusters, numAttributes, cluster_centres, tmp_cluster_centres)) {
-            printf("%s (%sreproducible) implementation not reproducible after %d runs!\n",
-                   parallel ? "Parallel" : "Sequential", reproducible ? "" : "non-", i);
-            break;
-        }
-    }
+    float **cluster_centres = NULL;
+    double time = omp_get_wtime();
+    cluster(parallel, reproducible, numObjects, numAttributes, attributes, /* [numObjects][numAttributes] */
+            nclusters, threshold, &cluster_centres);
+    time = omp_get_wtime() - time;
+    cout << fixed << setprecision(10) << (float) time * 1000.0 << '\t'; // ms
+    free(cluster_centres);
 }
 
 int main(int argc, char **argv)
@@ -129,169 +105,124 @@ int main(int argc, char **argv)
     extern char *optarg;
     extern int optind;
     int nclusters = 5;
-    char *filename = 0;
     float *buf;
     float **attributes;
-    float **cluster_centres_seq = NULL;
-    float **cluster_centres_omp = NULL;
-    float **cluster_centres_seq_rep = NULL;
-    float **cluster_centres_omp_rep = NULL;
-    int i, j;
 
-    int numAttributes;
-    int numObjects;
+    int numAttributes = 0;
+    int numObjects = 0;
     char line[1024];
-    int isBinaryFile = 0;
     int nrepeats = 0;
     float threshold = 0.001;
-    double time_seq;
-    double time_omp;
-    double time_seq_rep;
-    double time_omp_rep;
 
-    while ((opt = getopt(argc, argv, "i:k:r:t:b")) != EOF) {
-        switch (opt) {
-        case 'i':
-            filename = optarg;
-            break;
-        case 'b':
-            isBinaryFile = 1;
-            break;
-        case 't':
-            threshold = atof(optarg);
-            break;
-        case 'k':
-            nclusters = atoi(optarg);
-            break;
-        case 'r':
-            nrepeats = atoi(optarg);
-            if (nrepeats < 0) {
-                printf("Invalid number of repeats: %d!", nrepeats);
+    const int exe_path_len = strrchr(argv[0], '/') - argv[0] + 1;
+    char exe_path[256];
+    strncpy(exe_path, argv[0], exe_path_len);
+    exe_path[exe_path_len] = '\0';
+
+    char input_file_path[256];
+
+    cout << "unit: [ms]\n\n";
+
+    int nfiles = sizeof(input_files) / sizeof(input_files[0]);
+
+    for (int file_idx = 0; file_idx < nfiles; ++file_idx)
+    {
+        strncpy(input_file_path, exe_path, exe_path_len + 1);
+        strcat(input_file_path, "data/");
+        strcat(input_file_path, input_files[file_idx]);
+
+        cout << input_files[file_idx] << "\n";
+
+        /* from the input file, get the numAttributes and numObjects ------------*/
+        {
+            numObjects = 0;
+            numAttributes = 0;
+
+            FILE *infile;
+            if ((infile = fopen(input_file_path, "r")) == NULL) {
+                fprintf(stderr, "Error: no such file (%s)\n", input_file_path);
                 exit(1);
             }
-            break;
-        case '?':
-            usage(argv[0]);
-            break;
-        default:
-            usage(argv[0]);
-            break;
-        }
-    }
-
-    if (filename == 0)
-        usage(argv[0]);
-
-    numAttributes = numObjects = 0;
-
-    /* from the input file, get the numAttributes and numObjects ------------*/
-
-    if (isBinaryFile) {
-        int infile;
-        if ((infile = open(filename, O_RDONLY, "0600")) == -1) {
-            fprintf(stderr, "Error: no such file (%s)\n", filename);
-            exit(1);
-        }
-        read(infile, &numObjects, sizeof(int));
-        read(infile, &numAttributes, sizeof(int));
-
-        /* allocate space for attributes[] and read attributes of all objects */
-        buf = (float *) malloc(numObjects * numAttributes * sizeof(float));
-        attributes = (float **) malloc(numObjects * sizeof(float *));
-        attributes[0] = (float *) malloc(numObjects * numAttributes * sizeof(float));
-        for (i = 1; i < numObjects; i++)
-            attributes[i] = attributes[i - 1] + numAttributes;
-
-        read(infile, buf, numObjects * numAttributes * sizeof(float));
-
-        close(infile);
-    } else {
-        FILE *infile;
-        if ((infile = fopen(filename, "r")) == NULL) {
-            fprintf(stderr, "Error: no such file (%s)\n", filename);
-            exit(1);
-        }
-        while (fgets(line, 1024, infile) != NULL)
-            if (strtok(line, " \t\n") != 0)
-                numObjects++;
-        rewind(infile);
-        while (fgets(line, 1024, infile) != NULL) {
-            if (strtok(line, " \t\n") != 0) {
-                /* ignore the id (first attribute): numAttributes = 1; */
-                while (strtok(NULL, " ,\t\n") != NULL)
-                    numAttributes++;
-                break;
+            while (fgets(line, 1024, infile) != NULL)
+                if (strtok(line, " \t\n") != 0)
+                    numObjects++;
+            rewind(infile);
+            while (fgets(line, 1024, infile) != NULL) {
+                if (strtok(line, " \t\n") != 0) {
+                    /* ignore the id (first attribute): numAttributes = 1; */
+                    while (strtok(NULL, " ,\t\n") != NULL)
+                        numAttributes++;
+                    break;
+                }
             }
-        }
 
-        /* allocate space for attributes[] and read attributes of all objects */
-        buf = (float *) malloc(numObjects * numAttributes * sizeof(float));
-        attributes = (float **) malloc(numObjects * sizeof(float *));
-        attributes[0] = (float *) malloc(numObjects * numAttributes * sizeof(float));
-        for (i = 1; i < numObjects; i++)
-            attributes[i] = attributes[i - 1] + numAttributes;
-        rewind(infile);
-        i = 0;
-        while (fgets(line, 1024, infile) != NULL) {
-            if (strtok(line, " \t\n") == NULL)
-                continue;
-            for (j = 0; j < numAttributes; j++) {
-                buf[i] = atof(strtok(NULL, " ,\t\n"));
-                i++;
+            /* allocate space for attributes[] and read attributes of all objects */
+            buf = (float *) malloc(numObjects * numAttributes * sizeof(float));
+            attributes = (float **) malloc(numObjects * sizeof(float *));
+            attributes[0] = (float *) malloc(numObjects * numAttributes * sizeof(float));
+            int i;
+            for (i = 1; i < numObjects; i++)
+                attributes[i] = attributes[i - 1] + numAttributes;
+            rewind(infile);
+            i = 0;
+            while (fgets(line, 1024, infile) != NULL) {
+                if (strtok(line, " \t\n") == NULL)
+                    continue;
+                for (int j = 0; j < numAttributes; j++) {
+                    buf[i] = atof(strtok(NULL, " ,\t\n"));
+                    i++;
+                }
             }
+            fclose(infile);
         }
-        fclose(infile);
+
+        memcpy(attributes[0], buf, numObjects * numAttributes * sizeof(float));
+        free(buf);
+
+        cout << "\nseq\t";
+
+        for (int run = 0; run < 3; ++run) execute (false, false, numObjects, numAttributes, attributes, nclusters, threshold);
+        cout << '\n';
+        
+        for (int thread_count = 1; thread_count <= 128; thread_count <<= 1)
+        {
+            omp_set_dynamic(0);                 // Explicitly disable dynamic teams
+            omp_set_num_threads(thread_count);  // Use  thread_count threads for all consecutive parallel regions
+
+            #pragma omp parallel
+            #pragma omp single
+            {
+                cout << omp_get_num_threads() << '\t';
+            }
+
+            for (int run = 0; run < 3; ++run) execute (true, false, numObjects, numAttributes, attributes, nclusters, threshold);
+            cout << '\n';
+        }
+        
+        cout << "\nreproducible\n\nseq\t";
+
+        for (int run = 0; run < 3; ++run) execute (false, true, numObjects, numAttributes, attributes, nclusters, threshold);
+        cout << '\n';
+        
+        for (int thread_count = 1; thread_count <= 128; thread_count <<= 1)
+        {
+            omp_set_dynamic(0);                 // Explicitly disable dynamic teams
+            omp_set_num_threads(thread_count);  // Use  thread_count threads for all consecutive parallel regions
+
+            #pragma omp parallel
+            #pragma omp single
+            {
+                cout << omp_get_num_threads() << '\t';
+            }
+
+            for (int run = 0; run < 3; ++run) execute (true, true, numObjects, numAttributes, attributes, nclusters, threshold);
+            cout << '\n';
+        }
+
+        cout << '\n';
+
+        free(attributes);
     }
-
-    printf("I/O completed\n");
-    printf("  Number of clusters: %d\n", nclusters);
-    printf("  Number of objects: %d\n", numObjects);
-    printf("  Number of attributes: %d\n", numAttributes);
-    printf("  Number of available threads: %d\n", omp_get_max_threads());
-    printf("  Number of repeats: %d\n", nrepeats);
-
-    memcpy(attributes[0], buf, numObjects * numAttributes * sizeof(float));
-
-    run(nrepeats, false, false, numObjects, numAttributes, attributes, nclusters, threshold, cluster_centres_seq, time_seq);
-    run(nrepeats, false, true, numObjects, numAttributes, attributes, nclusters, threshold, cluster_centres_seq_rep, time_seq_rep);
-    run(nrepeats, true, false, numObjects, numAttributes, attributes, nclusters, threshold, cluster_centres_omp, time_omp);
-    run(nrepeats, true, true, numObjects, numAttributes, attributes, nclusters, threshold, cluster_centres_omp_rep, time_omp_rep);
-
-    printf("Non-reproducible sequential and parallel results %smatch!\n",
-           diff(nclusters, numAttributes, cluster_centres_seq, cluster_centres_omp) ? "do not " : "");
-
-    printf("Reproducible sequential and parallel results %smatch!\n",
-           diff(nclusters, numAttributes, cluster_centres_seq_rep, cluster_centres_omp_rep) ? "do not " : "");
-
-    if (diff(nclusters, numAttributes, cluster_centres_seq, cluster_centres_seq_rep)) {
-        printf("Non-reproducible and reproducible sequential results do not match!\n");
-    }
-
-    if (diff(nclusters, numAttributes, cluster_centres_omp, cluster_centres_omp_rep)) {
-        printf("Non-reproducible and reproducible parallel results do not match!\n");
-    }
-
-    printf("Sequential implementation time: %.3f\n", time_seq);
-    printf("Parallel implementation time: %.3f\n", time_omp);
-    printf("Speedup: %.3f\n", time_seq / time_omp);
-
-    printf("Sequential implementation time (reproducible): %.3f\n", time_seq_rep);
-    printf("Parallel implementation time (reproducible): %.3f\n", time_omp_rep);
-    printf("Speedup (reproducible): %.3f\n", time_seq_rep / time_omp_rep);
-
-    printf("Time sequential reproducible / non-reproducible: %.3f\n", time_seq_rep / time_seq);
-    printf("Time parallel reproducible / non-reproducible: %.3f\n", time_omp_rep / time_omp);
-
-    free(attributes);
-    free(cluster_centres_seq[0]);
-    free(cluster_centres_seq);
-    free(cluster_centres_omp[0]);
-    free(cluster_centres_omp);
-    free(cluster_centres_seq_rep[0]);
-    free(cluster_centres_seq_rep);
-    free(cluster_centres_omp_rep[0]);
-    free(cluster_centres_omp_rep);
-    free(buf);
 
     return 0;
 }
